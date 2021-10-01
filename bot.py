cd3c36ad50f666802c814b8984efdf0c5561ea41
Mohammad Alhamdan <muhammad.alhamdan@gmail.com>
2021-09-30 23:51:52 -0400

    Added io.BytesIO instead of processing directory

--- bot.py
+++ bot.py
@@ -2,11 +2,28 @@ import discord
 import os
 from quickselect_dl import face_utils
 from quickselect_dl import inference
+import io
+import PIL
+from PIL import Image
 
-TOKEN = 'token here'
+from face_crop import FaceCrop
+
+TOKEN = 'TOKEN HERE'
 IMAGE_TYPES = ["png", "jpeg", "jpg"]
 ACTIONS = ['gender', 'race', 'emotion', 'age']
 client = discord.Client()
+fc = FaceCrop()
+
+# Convert between PIL and Binary
+def convert_image(image, original_type):
+    if original_type == "PIL":
+        image_binary = io.BytesIO()
+        image.save(image_binary, 'PNG')
+        image_binary.seek(0)
+        return image_binary
+    if original_type == "bytes":
+        PIL_image = Image.open(image)
+        return PIL_image
 
 @client.event
 async def on_ready():
@@ -15,7 +32,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content[0:5] == '-crop':
-
+        
         if message.reference is not None:
             message = await message.channel.fetch_message(message.reference.message_id) 
 
@@ -29,24 +46,28 @@ async def on_message(message):
         else:
             content_type = message.attachments[0].content_type[6:]
             if content_type in IMAGE_TYPES:
+                # TODO: multiple faces
+                if message.content == '-crop face':
+                    pass
+
                 response = "Cropping..."
                 response = await message.channel.send(response)  
                 bot_response = await message.channel.fetch_message(response.id) # Preserve id of sent message for later use
                 # Save image
-                image_path = f"./processing_images/{message.id}.{content_type}"
+                # image_path = f"./processing_images/{message.id}.{content_type}"
+                image_path = io.BytesIO()
                 await message.attachments[0].save(image_path)
+                image_path.seek(0)
+                image_path = convert_image(image_path, "bytes")
                 
                 # Cropping and sending image to channel
-                result_path = f"./processing_images/{message.id}-cropped.png"
+                # result_path = f"./processing_images/{message.id}-cropped.png"
                 try:
-                    inference.run(image_path, result_path, model_no=1)
-                    await message.channel.send(file=discord.File(result_path))
+                    result = inference.run(image_path, model_no=6)
+                    binary_image = convert_image(result, "PIL")
+                    await message.channel.send(file=discord.File(fp=binary_image, filename=f"{message.id}-cropped.png"))
                     await bot_response.edit(content="Done!")
-                    # Cleanup directory afterwards
-                    os.remove(image_path)
-                    os.remove(result_path)
                 except RuntimeError:
-                    os.remove(image_path)
                     await message.channel.send("Something went wrong. Please try a different image.")
 
     if message.content[1:len(message.content)] in ACTIONS and message.content[0] == '-':
@@ -67,16 +88,19 @@ async def on_message(message):
                 response = "Analyzing..."
                 response = await message.channel.send(response)  
                 # Save image
-                image_path = f"./processing_images/{message.id}.{content_type}"
+                # image_path = f"./processing_images/{message.id}.{content_type}"
+                image_path = io.BytesIO()
                 await message.attachments[0].save(image_path)
+                image_path.seek(0)
+                image_path = convert_image(image_path, "bytes")
+                # fix png problem
+                image_path = image_path.convert('RGB')
         
                 try:
                     result = face_utils.face_information(image_path, action)
                     await message.reply(f"Person's {action} is {result}.")
                     # Cleanup directory afterwards
-                    os.remove(image_path)
                 except RuntimeError:
-                    os.remove(image_path)
                     await message.channel.send("Something went wrong. Please try a different image.")
 
         
