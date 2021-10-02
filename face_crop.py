aa2f0d9e10ae6ce4c2248e55d6ea6497599be116
Mohammad Alhamdan <muhammad.alhamdan@gmail.com>
2021-10-02 18:24:33 -0400

    Reorganized functions

--- face_crop.py
+++ face_crop.py
@@ -10,7 +10,7 @@ class FaceCrop(object):
     current_image = None
     to_be_cropped = {}
 
-    def __init__(self, current_image, to_be_cropped):
+    def __init__(self, current_image=None, to_be_cropped=[]):
 
         self.current_image = None
         self.to_be_cropped = {}
@@ -34,31 +34,36 @@ class FaceCrop(object):
             axarr[idx].title.set_text(f"{idx+1}")
             axarr[idx].imshow(image)
 
-        plot_image = self.fig2img(axarr)
+        plot_image = self.fig2img(f)
         # plot_image.show()
         return plot_image
     
+    # Input: PIL
+    # Output: PIL after segmentation
+    def face_crop_and_segment(self, image):
+            result_after_cropping = face_utils.find_face(image)
+            result_after_segmentation = inference.run(result_after_cropping)
+            return result_after_segmentation
+
     # Input: PIL Image
     # Output: 
     # 1. if one face detected, return segmentation of face
     # 2. if multiple, return prompt
     def detect(self, image, message_id):
+
         result = find_faces.find_instances(image)
         # Multiple faces
         if len(result) > 1:
-            prompt = self.prompt_plot(self, result)
+
+            prompt = self.prompt_plot(result)
             # Update dict to preserve for after user choice
             if message_id not in self.to_be_cropped.keys():
-                images_status = []
-                # Image only switched to True when user chooses in prompt
-                for image in result: images_status.append((image, False))
-                self.to_be_cropped[message_id] = images_status
-            return prompt
+                self.to_be_cropped[message_id] = result
+            return prompt, len(result)
 
         elif len(result) == 1:
-            result_after_cropping = find_faces.find_face(result)
-            result_after_segmentation = inference.run(result_after_cropping)
-            return result_after_segmentation
+            result_after_segmentation = self.face_crop_and_segment(result[0])
+            return result_after_segmentation, 0
 
 
    
